1b4d9c090f533b79d002b1de39f4ceb03470e4a6
Parkash suthar <parkashsuther@gmail.com>
2020-10-07 18:28:24 +0500

    first commit

---
+++ build/precache-manifest.c53c2436b2ab830b2d65d66d121ae64c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc88bc58679f5d75380952645e373a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61d1af1b411a99d13ad2",
+    "url": "/static/css/main.41bced14.chunk.css"
+  },
+  {
+    "revision": "1fb893235c94cf39bd2b",
+    "url": "/static/js/2.3eae5307.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/static/js/2.3eae5307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d1af1b411a99d13ad2",
+    "url": "/static/js/main.076397c3.chunk.js"
+  },
+  {
+    "revision": "10545e6b10910a0b9f12",
+    "url": "/static/js/runtime-main.025be44c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
